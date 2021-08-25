623c469791a7c3023277646655f47fb970f70d19
Vitaly Tihonov <v.tikhonov@gt-sot.ru>
2021-08-25 18:38:35 +0300

    Revert "Выполнил Basic instructions из spa-github-pages"
    This reverts commit 9d3201db6df41a967bbce1bbfba6b912c17e5dea.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/data-item-tree" >
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
