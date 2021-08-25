de8ed7decb0dda3ffbd6f2fe46960fb599613d56
Vitaly Tihonov <v.tikhonov@gt-sot.ru>
2021-08-25 18:14:26 +0300

    Update boilerplate for use with my domain

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/data-item-tree">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
